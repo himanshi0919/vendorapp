347b092482f511033f92905764abe1ca0f2f3fc6
Himanshi Agrawal <himanshiagrawal09199@gmail.com>
2023-06-25 11:43:36 +0530

    Fifth Commit

--- pages/profile.js
+++ pages/profile.js
@@ -2,33 +2,29 @@ import React from 'react';
 import Link from "next/link";
 import { getSession } from 'next-auth/react'
 
-const Profile = () => {
+export default () => {
     return (
         <section className="container mx-auto text-center">
-            <h3 className="text-4xl font-bold">Thank you!</h3>
-            <Link href="/login">Login</Link>
+                <h3 className="text-4xl font-bold">Thankyou!</h3>
+
+                <Link href={"/login"}>Login</Link>
         </section>
-    );
-};
+    )
+}
 
-Profile.displayName = 'Profile';
-
-export default Profile;
-
-export async function getServerSideProps({ req }) {
+export async function getServerSideProps({ req }){
     const session = await getSession({ req });
 
-    if (!session) {
+    if(!session){
         return {
-            redirect: {
-                destination: "/login",
-                permanent: false
+            redirect : {
+                destination : "/login",
+                premanent: false
             }
         };
     }
-
     // authorize user return session
     return {
         props: { session }
-    };
+    }
 }
